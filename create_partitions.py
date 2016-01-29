ff685d381ea06518e94b6a82150931dd4615f3be
Abhinav Baid <abhinavbaid@gmail.com>
2016-01-29 23:22:07 +0530

    Implement create_partitions

---
+++ create_partitions.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  create_partitions.py
+
+def create_partitions(G, k, p):
+    partitions = []
+    free = []
+    n = len(p)
+    for i in range(k):
+        partitions.append([p[i]])
+        free.append(False)
+    for i in range(k, n):
+        free.append(True)
+    someFree = True
+    while someFree:
+        someFree = False
+        for i in range(k, n):
+            if free[i]:
+                smallest = float('inf')
+                index = -1
+                for l in range(k):
+                    for j in G[p[i]]:
+                        if j in partitions[l]:
+                            if len(partitions[l]) < smallest:
+                                smallest = len(partitions[l])
+                                index = l
+                if index == -1:
+                    someFree = True
+                else:
+                    partitions[index].append(p[i])
+                    free[i] = False
+    return partitions
+
+def main(args):
+    G = [[1,2,3],[0,7,8],[0,3,7],[0,2,4,5],[3,5],[3,4,7],[7,8,10],[1,2,5,6],[1,6,9],[8,11],[6,11],[9,10]]
+    k = 3
+    p = [6,5,2,9,4,0,10,8,3,11,7,1]
+    print create_partitions(G, k, p)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
