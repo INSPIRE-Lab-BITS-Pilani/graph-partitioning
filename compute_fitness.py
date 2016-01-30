63146aa37f9501b46b43e7f639511012b8333093
Abhinav Baid <abhinavbaid@gmail.com>
2016-01-30 23:38:07 +0530

    Implement compute_fitness

---
+++ compute_fitness.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+#
+#  compute_fitness.py
+
+from create_partitions import create_partitions
+
+def compute_fitness(G, k, P):
+    fitness = {}
+    for p in P:
+        parts = create_partitions(G, k, p)
+        fitness[p] = float(k) * max([len(part) for part in parts]) / len(G)
+    return fitness
+
+def main(args):
+    G = [[1,2,3],[0,7,8],[0,3,7],[0,2,4,5],[3,5],[3,4,7],[7,8,10],[1,2,5,6],[1,6,9],[8,11],[6,11],[9,10]]
+    k = 3
+    P = [(6,5,2,9,4,0,10,8,3,11,7,1)]
+    print compute_fitness(G, k, P)
+    return 0
+
+if __name__ == '__main__':
+    import sys
+    sys.exit(main(sys.argv))
